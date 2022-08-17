d495359983b611a851babc65a1849c3826fc8f92
pierre <pierre.pontic@gmail.com>
2022-08-17 18:58:31 +0200

    add fuzz to metal material factorise RandomInUnitSphere function

--- Raytrace/Material.cs
+++ Raytrace/Material.cs
@@ -1,7 +1,28 @@
-﻿namespace RayTrace
+﻿using System;
+
+namespace RayTrace
 {
     public abstract class Material
     {
+        private readonly Random _random;
+        public Material()
+            : this(new Random())
+        {
+
+        }
+        public Material(Random random)
+        {
+            _random = random;
+        }
+        public Vector3 RandomInUnitSphere()
+        {
+            var p = new Vector3(0, 0, 0);
+            do
+            {
+                p = 2f * new Vector3((float)_random.NextDouble(), (float)_random.NextDouble(), (float)_random.NextDouble()) - new Vector3(1, 1, 1);
+            } while (Vector3.Dot(p, p) >= 1);
+            return p;
+        }
         public abstract bool Scatter(Ray r, HitRecord record, out Vector3 attenuation, out Ray scattered);
     }
 }
--- Raytrace/Metal.cs
+++ Raytrace/Metal.cs
@@ -3,7 +3,7 @@
     public class Metal : Material
     {
         public Vector3 Albedo { get; set; }
-
+        public float Fuzz { get; set; }
         public Vector3 Reflect(Vector3 v, Vector3 n)
         {
             return v - 2 * Vector3.Dot(v, n) * n;
@@ -12,7 +12,7 @@
         public override bool Scatter(Ray r, HitRecord record, out Vector3 attenuation, out Ray scattered)
         {
             var reflected = Reflect(Vector3.UnitVector(r.Direction), record.normal);
-            scattered = new Ray(record.p, reflected);
+            scattered = new Ray(record.p, reflected + Fuzz * RandomInUnitSphere());
             attenuation = Albedo;
             return Vector3.Dot(scattered.Direction, record.normal) > 0;
         }
