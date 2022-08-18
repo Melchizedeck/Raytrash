d58cbd210835d9a747815c98efa98bf63d4de069
pierre <pierre.pontic@gmail.com>
2022-08-18 10:38:01 +0200

    correct ThreadSafeRandom generation

--- Raytrace/Material.cs
+++ Raytrace/Material.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 
 namespace RayTrace
 {
@@ -20,24 +19,5 @@ namespace RayTrace
             return p;
         }
         public abstract bool Scatter(Ray r, HitRecord record, out Vector3 attenuation, out Ray scattered);
-    }
-
-    public class ThreadSafeRandom
-    {
-        private static Random _global = new Random((int)DateTime.Now.Ticks);
-        [ThreadStatic]
-        private static Random _local;
-        public static double NextDouble()
-        {
-            var inst = _local;
-            if (inst == null)
-            {
-                int seed;
-                lock (_global) return _global.NextDouble();
-                _local = new Random(seed);
-            }
-            return _local.NextDouble();
-
-        }
     }
 }
---
+++ Raytrace/ThreadSafeRandom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RayTrace
+{
+    public class ThreadSafeRandom
+    {
+        private static Random _global = new Random((int)DateTime.Now.Ticks);
+        [ThreadStatic]
+        private static Random _local;
+        public static double NextDouble()
+        {
+            var inst = _local;
+            if (inst == null)
+            {
+                int seed;
+                lock (_global) { seed = _global.Next(); }
+                _local = new Random(seed);
+            }
+            return _local.NextDouble();
+
+        }
+    }
+}
